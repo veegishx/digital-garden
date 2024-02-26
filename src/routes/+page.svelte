184f0a8eb102178d6c5e1cf8b712bf04f9d62204
Veegish Ramdani <veegish240@gmail.com>
2024-02-26 22:45:46 +0400

    feat: Add styling for latest posts layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -87,31 +87,48 @@
 
 <section class="latest-posts">
 	<h2 class="latest-posts__label">Latest Posts</h2>
-	<ul class="article-list">
+	<ul class="latest-posts__article-list">
 		{#each data.homePagePosts.posts as post}
-			<li class="latest-posts__article">
-				<a href={post.slug}>
+			<li class="latest-posts__article-list-item">
+				<a
+					href={post.slug}
+					class="latest-posts__article-list-item--link"
+					style={post.thumbnail ? 'flex-direction: row' : 'flex-direction: column'}
+				>
 					<!-- <p class="date">{formatDate(post.date)}</p> -->
 					{#if post.thumbnail}
-						<h2 class="latest-posts__article-title">{post.title}</h2>
 						<img class="latest-posts__article-thumbnail" src={post.thumbnail} alt={post?.title} />
+						<div class="latest-posts__article-meta">
+							<h2 class="latest-posts__article-title">{post.title}</h2>
+							<p class="latest-posts__article-description">
+								{post.description.length >= 400
+									? post.description.substring(0, 600).concat('...')
+									: post.description}
+							</p>
+							<p class="latest-posts__article-author">
+								{post.author} - {new Date(post.date).toLocaleString('en-GB', {
+									day: '2-digit',
+									year: 'numeric',
+									month: 'long'
+								})}
+							</p>
+						</div>
 					{/if}
 					{#if !post.thumbnail}
 						<h2 class="latest-posts__article-title--primary">{post.title}</h2>
+						<p class="latest-posts__article-description">
+							{post.description.length >= 400
+								? post.description.substring(0, 600).concat('...')
+								: post.description}
+						</p>
+						<p class="latest-posts__article-author">
+							{post.author} - {new Date(post.date).toLocaleString('en-GB', {
+								day: '2-digit',
+								year: 'numeric',
+								month: 'long'
+							})}
+						</p>
 					{/if}
-
-					<p class="latest-posts__article-description">
-						{post.description.length >= 400
-							? post.description.substring(0, 600).concat('...')
-							: post.description}
-					</p>
-					<p class="latest-posts__article-author">
-						{post.author} - {new Date(post.date).toLocaleString('en-GB', {
-							day: '2-digit',
-							year: 'numeric',
-							month: 'long'
-						})}
-					</p>
 				</a>
 			</li>
 		{/each}
@@ -140,7 +157,7 @@
 		text-transform: uppercase;
 		font-size: 18px;
 		font-family: var(--font-mono);
-		padding: 20px;
+		padding: 10px;
 		text-decoration: underline;
 	}
 	.above-fold-grid__left-column .article-list {
@@ -242,7 +259,178 @@
 		display: none;
 	}
 
-	/** Small article cards */
+	a {
+		color: unset;
+	}
+	a:visited {
+		color: unset;
+	}
+	a:hover .featured-post__title {
+		text-decoration: underline;
+		text-underline-offset: auto;
+	}
+	a:hover,
+	a:hover .featured-post__description {
+		text-decoration: none;
+	}
+	.latest-posts {
+		padding-block: var(--size-9);
+		padding-left: 25px;
+		padding-right: 25px;
+	}
+
+	.featured-post {
+		padding-left: 25px;
+		padding-right: 25px;
+	}
+
+	.featured-post,
+	.featured-post__cover {
+		position: relative;
+	}
+
+	.featured-post__header {
+		display: flex;
+		gap: 10px;
+	}
+
+	.featured-post__icon {
+		font-family: var(--font-mono);
+		display: flex;
+		gap: 10px;
+		width: fit-content;
+		padding: 8px 12px;
+		margin-bottom: 15px;
+		text-transform: uppercase;
+		font-size: 14px;
+		font-weight: 600;
+		background: var(--featured-tag-bg);
+		color: var(--featured-tag-fg);
+	}
+
+	.featured-post__category {
+		width: fit-content;
+		border: 1px solid;
+		padding: 8px 12px;
+		margin-bottom: 15px;
+		text-transform: uppercase;
+		font-size: 14px;
+		font-family: var(--font-mono);
+	}
+
+	.featured-post__date {
+		background: black;
+		color: white;
+		padding: 4px 8px 4px 8px;
+		width: fit-content;
+		font-size: 12px;
+	}
+
+	.featured-post__meta {
+		margin: 10px auto;
+	}
+
+	.featured-post__thumbnail {
+		border-radius: 0;
+	}
+
+	.featured-post__title {
+		font-size: 26px;
+		font-family: var(--font-bebas-neue);
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-rendering: optimizeLegibility;
+		text-transform: uppercase;
+		max-inline-size: unset;
+		text-wrap: wrap;
+	}
+
+	.featured-post__description {
+		font-size: 14px;
+		margin-bottom: 20px;
+	}
+
+	.featured-post__author {
+		font-size: 12.5px;
+		font-family: var(--font-mono);
+	}
+
+	.latest-posts {
+		display: flex;
+		flex-direction: column;
+		overflow: hidden;
+		position: relative;
+		margin: 0 auto;
+		padding: 30px;
+		max-width: 1600px;
+		width: 100%;
+		justify-content: center;
+		align-items: center;
+		gap: 25px;
+	}
+
+	.latest-posts__label {
+		font-family: var(--font-bebas-neue);
+	}
+
+	.latest-posts__article-list-item {
+		max-inline-size: unset;
+		display: flex;
+		flex-direction: column;
+		margin-bottom: 20px;
+		overflow: unset;
+		width: 100%;
+		margin: 0px auto;
+	}
+
+	.latest-posts__article-meta {
+		max-inline-size: unset;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	.latest-posts__article-list-item:not(:last-child) {
+		border-bottom: 1px solid var(--articles-br);
+		margin-bottom: 20px;
+	}
+
+	.latest-posts__article-title {
+		font-size: var(--font-size-fluid-1);
+		max-inline-size: unset;
+		text-wrap: wrap;
+		margin-bottom: 20px;
+	}
+
+	.latest-posts__article-title--primary {
+		font-weight: 600;
+		font-size: var(--font-size-fluid-2);
+		margin-bottom: 20px;
+		max-inline-size: unset;
+		text-wrap: pretty;
+	}
+
+	.latest-posts__article-thumbnail {
+		margin-bottom: 20px;
+	}
+	.latest-posts__article-description {
+		font-size: 1rem;
+		margin-bottom: 20px;
+	}
+
+	.latest-posts__article-author {
+		font-size: 12.5px;
+		font-family: var(--font-mono);
+		margin-bottom: 20px;
+	}
+
+	.date {
+		color: var(--text-2);
+	}
+
+	.description {
+		margin-top: var(--size-3);
+	}
 
 	@media (min-width: 768px) {
 		.above-fold-grid {
@@ -269,7 +457,6 @@
 		.above-fold-grid__right-column {
 			grid-column: 1/-1;
 			border-top: 1px solid var(--articles-br);
-			border-bottom: 1px solid var(--articles-br);
 		}
 
 		.above-fold-grid__right-column ul {
@@ -284,7 +471,7 @@
 			display: inline;
 			background-color: var(--articles-br);
 			content: '';
-			height: 70%;
+			height: 80%;
 			position: absolute;
 			top: 5%;
 			left: calc((100dvw / 3) - 20px);
@@ -294,7 +481,7 @@
 			display: inline;
 			background-color: var(--articles-br);
 			content: '';
-			height: 70%;
+			height: 80%;
 			position: absolute;
 			top: 5%;
 			right: calc((100dvw / 3) - 20px);
@@ -364,6 +551,17 @@
 		.article-list__item-col h2,
 		.article-list__item-col .article-list__item-author {
 			padding: unset;
+		}
+
+		.latest-posts__article-list-item--link {
+			display: flex;
+			flex-direction: row;
+			gap: 20px;
+		}
+
+		.latest-posts__article-list-item--link img {
+			max-width: 300px;
+			max-height: 300px;
 		}
 	}
 	@media (min-width: 1024px) {
@@ -467,164 +665,5 @@
 			padding: 15px;
 			border: 1px solid;
 		}
-	}
-
-	a {
-		color: unset;
-	}
-	a:visited {
-		color: unset;
-	}
-	a:hover .featured-post__title {
-		text-decoration: underline;
-	}
-	a:hover,
-	a:hover .featured-post__description {
-		text-decoration: none;
-	}
-	.latest-posts {
-		padding-block: var(--size-9);
-		padding-left: 25px;
-		padding-right: 25px;
-	}
-	/* @media (min-width: 780px) {
-		.latest-posts {
-			display: grid;
-			grid-template-columns: 1fr 1fr 1fr;
-			grid-template-rows: 1fr 1fr 1fr;
-			gap: 0px 0px;
-			grid-template-areas:
-				'featured-post featured-post featured-post'
-				'latest-posts latest-posts latest-posts'
-				'latest-posts latest-posts latest-posts';
-		}
-		.featured-post {
-			grid-area: featured-post;
-		}
-		.latest-posts {
-			grid-area: latest-posts;
-		}
-	} */
-
-	.featured-post {
-		padding-left: 25px;
-		padding-right: 25px;
-	}
-
-	.featured-post,
-	.featured-post__cover {
-		position: relative;
-	}
-
-	.featured-post__header {
-		display: flex;
-		gap: 10px;
-	}
-
-	.featured-post__icon {
-		font-family: var(--font-mono);
-		display: flex;
-		gap: 10px;
-		width: fit-content;
-		padding: 8px 12px;
-		margin-bottom: 15px;
-		text-transform: uppercase;
-		font-size: 14px;
-		font-weight: 600;
-		background: var(--featured-tag-bg);
-		color: var(--featured-tag-fg);
-	}
-
-	.featured-post__category {
-		width: fit-content;
-		border: 1px solid;
-		padding: 8px 12px;
-		margin-bottom: 15px;
-		text-transform: uppercase;
-		font-size: 14px;
-		font-family: var(--font-mono);
-	}
-
-	.featured-post__date {
-		background: black;
-		color: white;
-		padding: 4px 8px 4px 8px;
-		width: fit-content;
-		font-size: 12px;
-	}
-
-	.featured-post__meta {
-		margin: 10px auto;
-	}
-
-	.featured-post__thumbnail {
-		border-radius: 0;
-	}
-
-	.featured-post__title {
-		font-size: 26px;
-		font-family: var(--font-bebas-neue);
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-rendering: optimizeLegibility;
-		text-transform: uppercase;
-		max-inline-size: unset;
-		text-wrap: wrap;
-	}
-
-	.featured-post__description {
-		font-size: 14px;
-		margin-bottom: 20px;
-	}
-
-	.featured-post__author {
-		font-size: 12.5px;
-		font-family: var(--font-mono);
-	}
-
-	.latest-posts__label {
-		font-family: var(--font-bebas-neue);
-	}
-
-	.latest-posts__article {
-		max-inline-size: unset;
-	}
-
-	.latest-posts__article:not(:last-child) {
-		border-bottom: 1px solid var(--articles-br);
-	}
-
-	.latest-posts__article-title {
-		font-size: var(--font-size-fluid-1);
-		max-inline-size: unset;
-		text-wrap: wrap;
-		margin-bottom: 20px;
-	}
-
-	.latest-posts__article-title--primary {
-		font-weight: 600;
-		font-size: var(--font-size-fluid-2);
-		margin-bottom: 20px;
-	}
-
-	.latest-posts__article-thumbnail {
-		margin-bottom: 20px;
-	}
-	.latest-posts__article-description {
-		font-size: 1rem;
-		margin-bottom: 20px;
-	}
-
-	.latest-posts__article-author {
-		font-size: 12.5px;
-		font-family: var(--font-mono);
-	}
-
-	.date {
-		color: var(--text-2);
-	}
-
-	.description {
-		margin-top: var(--size-3);
 	}
 </style>
